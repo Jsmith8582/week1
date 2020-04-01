825c9d0dd17d729d3c4069f235aa852dc52a6e71
Josh Smith <Jsmith8582@gmail.com>
2020-04-01 09:57:51 -0600

    second commit

--- src/App.java
+++ src/App.java
@@ -17,18 +17,23 @@ public class App {
 		String nickname = "Josh";
 		String lastname = "Smith";
 		char middleinitial = 'D';
+		String FoodStaple = "Beans and Rice";
+		
 		
 		//#4 operations using above variables
 		
 		double Wallet = fridaymoney - rice;
-		double Totalfriends = adultfriends / (currentage - childage);	
-		String FullName = firstname + " " + middleinitial + " " + lastname;
 		System.out.println(Wallet);
+		
+		double Totalfriends = adultfriends / (currentage - childage);	
 		System.out.println(Totalfriends);
+		
+		String FullName = firstname + " " + middleinitial + ". " + lastname;
 		System.out.println(FullName);
+		
 				
-		
-		
+		System.out.println("My favorite food is " + FoodStaple);
+		System.out.println("The amount of money in my wallet always seems to be " + mondaymoney + " dollars.");
 		
 		
 		
